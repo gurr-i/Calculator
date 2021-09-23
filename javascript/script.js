8737f1924beb32207d40f194ff26602704878373
Gopal <73361014+gurr-i@users.noreply.github.com>
2021-09-23 17:41:34 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,121 @@
+(function() {
+    "use strict";
+    var el = function(element) {
+        if (element.charAt(0) === "#") {
+            return document.querySelector(element);
+        }
+
+        return document.querySelectorAll(element);
+    };
+
+    // Variables
+    var viewer = el("#viewer"),
+        equals = el("#equals"),
+        nums = el(".num"),
+        ops = el(".ops"),
+        theNum = "",
+        oldNum = "",
+        resultNum,
+        operator;
+
+    var setNum = function() {
+        if (resultNum) {
+            theNum = this.getAttribute("data-num");
+            resultNum = "";
+        } else {
+            theNum += this.getAttribute("data-num");
+        }
+
+        viewer.innerHTML = theNum;
+
+    };
+
+    var moveNum = function() {
+        oldNum = theNum;
+        theNum = "";
+        operator = this.getAttribute("data-ops");
+
+        equals.setAttribute("data-result", "");
+    };
+
+    var displayNum = function() {
+
+        oldNum = parseFloat(oldNum);
+        theNum = parseFloat(theNum);
+
+        switch (operator) {
+            case "plus":
+                resultNum = oldNum + theNum;
+                break;
+
+            case "minus":
+                resultNum = oldNum - theNum;
+                break;
+
+            case "mult":
+                resultNum = oldNum * theNum;
+                break;
+
+            case "div":
+                resultNum = oldNum / theNum;
+                break;
+
+            case "pers":
+                resultNum = (oldNum / theNum) * 100;
+
+            case "negpos":
+                oldNum = "-" + oldNum;
+
+            default:
+                resultNum = theNum;
+        }
+
+        if (!isFinite(resultNum)) {
+            if (isNaN(resultNum)) {
+                resultNum = "NaN";
+            } else {
+                resultNum = "Look at what you've done";
+                el('#calculator').classList.add("broken");
+                el('#reset').classList.add("show");
+            }
+        }
+
+        viewer.innerHTML = resultNum;
+        equals.setAttribute("data-result", resultNum);
+
+        oldNum = 0;
+        theNum = resultNum;
+
+    };
+
+    var clearAll = function() {
+        oldNum = "";
+        theNum = "";
+        viewer.innerHTML = "0";
+        equals.setAttribute("data-result", resultNum);
+    };
+    var clear = function() {
+        resultNum = 0;
+        document.viewer = resultNum;
+    }
+
+    for (var i = 0, l = nums.length; i < l; i++) {
+        nums[i].onclick = setNum;
+    }
+
+    // Add click event to operators
+    for (var i = 0, l = ops.length; i < l; i++) {
+        ops[i].onclick = moveNum;
+    }
+
+    equals.onclick = displayNum;
+
+    el("#Allclear").onclick = clearAll;
+
+    el("#clear").onclick = clearAll;
+
+    el("#reset").onclick = function() {
+        window.location = window.location;
+    };
+
+}());
